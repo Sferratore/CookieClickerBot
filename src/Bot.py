388144f2cb46c76d82c6a0963b810ba71aa926e1
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-10-20 11:21:41 +0200

    Added positioning function

--- src/Bot.py
+++ src/Bot.py
@@ -4,3 +4,9 @@ class Bot:
 
     def get_display_size(self):
         return pyautogui.size().__str__()
+
+    def position_on_cookie(self):
+        if self.get_display_size() == "Size(width=1920, height=1080)":
+            pyautogui.moveTo(250, 500, duration=1)
+        else:
+            raise ValueError("Your screen size is not supported by this software!")
---
+++ src/Main.py
@@ -0,0 +1,11 @@
+from Bot import Bot
+
+
+def main():
+    b = Bot()
+    b.position_on_cookie()
+
+
+# Eseguire la funzione main solo se lo script viene eseguito direttamente
+if __name__ == "__main__":
+    main()
