acb46fb8c90b39480734e759f6e1d40545313c47
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-11-02 01:16:27 +0100

    Corrected bug on execute_bot: async not requested

--- src/Bot.py
+++ src/Bot.py
@@ -34,7 +34,7 @@ class Bot:
             raise Exception("Cookie Clicker window not found.")
 
 
-    async def execute_bot(self):
+    def execute_bot(self):
         self.position_window()
         self.position_on_cookie()
         while True:
